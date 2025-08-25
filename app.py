4575d4845b7af204759e746dd9c1e3f54a721853
Justin Lim <justin.lim@salesforce.com>
2025-08-25 10:57:34 +0800

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to Justin site"
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
